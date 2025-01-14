2e756e6a171b6483b0f7d5bce3d3e854a7d1d069
Sanskriti-Khanal <sanskritikhanal5@gmail.com>
2025-01-14 15:00:00 +0545

    feat: add remote datasource interface to auth datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:cineghar/features/auth/data/models/auth_api_model.dart';
 import 'package:cineghar/features/auth/data/models/auth_hive_model.dart';
 
 abstract interface class IAuthDatasource {
@@ -6,5 +7,11 @@ abstract interface class IAuthDatasource {
   Future<AuthHiveModel?> getCurrentUser();
   Future<bool> logout();
   Future<bool> isEmailExists(String email);
+}
+
+abstract interface class IAuthRemoteDataSource {
+  Future<AuthApiModel> register(AuthApiModel user);
+  Future<AuthApiModel?> login(String email, String password);
+  Future<AuthApiModel?> getUserById(String authId);
 }
 

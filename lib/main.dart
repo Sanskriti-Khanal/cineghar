de6881797631fcbb017dfd4b732dc512a157c909
Sanskriti-Khanal <sanskritikhanal5@gmail.com>
2025-01-15 10:00:00 +0545

    feat: initialize SharedPreferences in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:cineghar/app/app.dart';
+import 'package:cineghar/core/providers/shared_prefs_provider.dart';
 import 'package:cineghar/core/services/hive/hive_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Initialize Hive
   final hiveService = HiveService();
   await hiveService.init();
 
-  runApp(const ProviderScope(child: App()));
+  // Initialize SharedPreferences
+  final sharedPrefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPrefs),
+      ],
+      child: const App(),
+    ),
+  );
 }

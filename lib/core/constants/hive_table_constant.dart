541017d99c37ae49378bf9d984cd4fd3855ffb09
Sanskriti-Khanal <sanskritikhanal5@gmail.com>
2026-01-02 17:59:27 +0545

    Add db version constant

--- lib/core/constants/hive_table_constant.dart
+++ lib/core/constants/hive_table_constant.dart
@@ -2,6 +2,7 @@ class HiveTableConstant {
   HiveTableConstant._();
 
   static const String dbName = "cineghar_db";
+  static const int dbVersion = 1;
 
   static const int authTypeId = 0;
   static const String authTable = "auth_table";
